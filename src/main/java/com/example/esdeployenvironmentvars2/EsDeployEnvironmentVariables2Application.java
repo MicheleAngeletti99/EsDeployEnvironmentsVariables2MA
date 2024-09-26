e71c55c3f63af25c9993d9a8bb20f43e1b05b12b
Michele Angeletti <m.angeletti99@gmail.com>
2024-09-26 12:40:53 +0200

    Esercizio

---
+++ src/main/java/com/example/esdeployenvironmentvars2/EsDeployEnvironmentVariables2Application.java
@@ -0,0 +1,13 @@
+package com.example.esdeployenvironmentvars2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsDeployEnvironmentVariables2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsDeployEnvironmentVariables2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/esdeployenvironmentvars2/EsDeployEnvironmentVariables2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.esdeployenvironmentvars2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsDeployEnvironmentVariables2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
